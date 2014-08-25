bf527a0cdc5408fb106ed470209bc1fa44a06233
jokersingh <sakhibarun@gmail.com>
2014-08-25 15:03:32 +0530

    angularjs pages

--- src/main/webapp/resources/js/App.js
+++ src/main/webapp/resources/js/App.js
@@ -2,14 +2,21 @@
  * 
  */
 (function(){
-	var app = angular.module('login',[]);
-	
-	app.controller('EMSController',['$http',function($http){
-		var login = this;
-		login.employees=[];
-		$http.get('http://localhost:8080/ems/employees').success(function(data){
-			login.employees=data;
-		});
+	var app = angular.module('home',[]);
+	app.directive('employeeTable',['$http',function($http){
+		return {
+			restrict : 'E',
+			templateUrl : "employee-table.html",
+			controller : function($http){
+				var login = this;
+				login.employees=[];
+				$http.get('http://localhost:8080/ems/employees').success(function(data){
+					login.employees=data;
+				});
+			},
+			controllerAs : 'emsctrl'
+		}
+		
 	}]);
 	
 })();
